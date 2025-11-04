6a25e34cfe7f1bf1c9a3d3094c4b3e9cbcc3023b
aido132516 <aido132516@gmail.com>
2025-11-04 14:23:26 -0600

    Add files via upload
    Construction Group Agent Outlined Deliverable 3

---
+++ _alp/Agents/CostructionGroup/Code/Events.java
@@ -0,0 +1,8 @@
+void evaluateMarket()
+{/*ALCODESTART::1762284202594*/
+for (/*type of house in market (based on size)*/)
+	if (/*cost to build is < market's selling price for house && current budget of company > cost to build*/)
+		// Send a new house of this type through discrete event pipeline
+
+/*ALCODEEND*/}
+
