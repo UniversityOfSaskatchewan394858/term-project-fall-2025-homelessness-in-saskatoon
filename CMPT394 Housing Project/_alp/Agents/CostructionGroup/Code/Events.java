92db9b28e9851d24755b0db1466c25a0efadd5a0
aido132516 <aido132516@gmail.com>
2025-11-23 16:05:09 -0600

    Construction DES coded
    Made code pieces for event and pipeline to add new agents into the market population housesForSale

--- CMPT394 Housing Project/_alp/Agents/CostructionGroup/Code/Events.java
+++ CMPT394 Housing Project/_alp/Agents/CostructionGroup/Code/Events.java
@@ -1,8 +1,14 @@
 void evaluateMarket()
 {/*ALCODESTART::1762284202594*/
-for (/*type of house in market (based on size)*/)
-	if (/*cost to build is < market's selling price for house && current budget of company > cost to build*/)
-		// Send a new house of this type through discrete event pipeline
-
+for (House house : get_Main().market.housesForSale){
+	if (house.sizeOfHouse*pricePerSqft < house.sizeOfHouse*get_Main().market.salePricePerMaintenanceSize
+	 && totalBudgetOfGroup > house.sizeOfHouse*pricePerSqft){
+		
+		totalBudgetOfGroup -= house.sizeOfHouse*pricePerSqft;
+		houseConstruction.take(new House(0.0, house.sizeOfHouse,
+		house.sizeOfHouse*get_Main().market.salePricePerMaintenanceSize, 
+		house.sizeOfHouse*get_Main().market.salePricePerMaintenanceSize*0.1));
+	}
+}
 /*ALCODEEND*/}
 
